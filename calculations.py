ce104b0f165c7f219c45297eabc5470d40a8e66e
Will <whou125@gmail.com>
2018-01-06 17:05:02 -0800

    Fixed issue where all functions in calculations.py were called when imported by chi_square.py

--- calculations.py
+++ calculations.py
@@ -2,54 +2,57 @@ import csv
 from matplotlib import pyplot as plt
 from scipy import stats
 
-
-# Generates list of x(gas) coordinates and y(cci) coordinates
-def create_scatter(gas_data, cci_data):
-    gas_x = []
-    cci_y = []
-    counter = 0
-
-    for row in gas_data:
-        if counter != 0:
-            # Add values to x(gas) list and converts them to floats
-            gas_x.extend([float(i) for i in row[1:]])
-        counter += 1
-    counter = 0
-    for row in cci_data:
-        if counter != 0:
-            # Add values to y(cci) list and converts them to floats
-            cci_y.extend([float(i) for i in row[6:7]])
-        counter += 1
-    return (gas_x, cci_y)
-
-
-# Plots gas price each month against CCI
-def plot_data(gas_x, cci_y):
-    plt.xlabel("Gas Prices($)")
-    plt.ylabel("Consumer Confidence Index")
-    plt.title("Relationships between Gas Prices and the Consumer Confidence Index", weight='bold', y=1.02,
-              fontsize=13)
-    plt.text(3, 102, "r= ", fontsize=14)
-    plt.text(3.15, 102, r, fontsize=14)
-    plt.scatter(gas_x, cci_y)
-    plt.show();
-
-
-def get_pearsonr(gas_x, cci_y):
-    r, p = stats.pearsonr(gas_x, cci_y)
-    return r, p
-
-
 with open("data/gas.csv", 'r') as gascsv, open("data/cci.csv", 'r') as ccicsv:
     readgas = csv.reader(gascsv)
     readcci = csv.reader(ccicsv)
+
+
+    # Generates list of x(gas) coordinates and y(cci) coordinates
+    def prepare_scatter(gas_data=readgas, cci_data=readcci):
+        gas_x = []
+        cci_y = []
+        counter = 0
+
+        for row in gas_data:
+            if counter != 0:
+                # Add values to x(gas) list and converts them to floats
+                gas_x.extend([float(i) for i in row[1:]])
+            counter += 1
+        counter = 0
+        for row in cci_data:
+            if counter != 0:
+                # Add values to y(cci) list and converts them to floats
+                cci_y.extend([float(i) for i in row[6:7]])
+            counter += 1
+        return (gas_x, cci_y)
+
+
+    # Plots gas price each month against CCI
+    def plot_data(gas_price, cci_value):
+        plt.xlabel("Gas Prices($)")
+        plt.ylabel("Consumer Confidence Index")
+        plt.title("Relationships between Gas Prices and the Consumer Confidence Index", weight='bold', y=1.02,
+                  fontsize=13)
+        plt.text(3, 102, "r= ", fontsize=14)
+        plt.text(3.15, 102, r, fontsize=14)
+        plt.scatter(gas_price, cci_value)
+        plt.show()
+
+
+    def get_pearsonr(gas_price, cci_value):
+        r, p = stats.pearsonr(gas_price, cci_value)
+        return r, p
+
+
     # Prepares raw data into two lists: gas_x and cci_y
-    gas_x, cci_y = create_scatter(readgas, readcci)
-    # Calculates the Pearson Correlation Coefficient
-    r, p = get_pearsonr(gas_x, cci_y)
-    # Plot gas price against cci index
-    plot_data(gas_x, cci_y)
+    gas_x, cci_y = prepare_scatter()
+
+    if __name__ == "__main__":
+        # Calculates the Pearson Correlation Coefficient
+        r, p = get_pearsonr(gas_x, cci_y)
+        # Plot gas price against cci index
+        plot_data(gas_x, cci_y)
 
 
-def created_scatter():
-    return gas_x, cci_y
+    def created_scatter():
+        return gas_x, cci_y
--- chi_square.py
+++ chi_square.py
@@ -4,12 +4,12 @@ from scipy import stats
 gas_x, cci_y = created_scatter()
 
 
-def create_contingency(gas_x=gas_x, cci_y=cci_y):
+def create_contingency(gas_price=gas_x, cci_value=cci_y):
     quartiles = [0, 25, 50, 75, 100]
     quartile_values = []
     for i in range(0, 5):
-        quartile_values.append(stats.scoreatpercentile(gas_x, quartiles[i]))
-    print quartile_values
+        quartile_values.append(stats.scoreatpercentile(gas_price, quartiles[i]))
+    print(quartile_values)
 
 
 create_contingency()
